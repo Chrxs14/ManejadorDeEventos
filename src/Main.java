1a7eae1bfc24245885d695f24d13bfe797476939
chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-04-28 00:44:55 -0500

    Updated: Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import GUI.Window;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
